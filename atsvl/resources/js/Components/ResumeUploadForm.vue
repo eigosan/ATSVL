7b32674721c27a23935efcd3225e8fa1ffa5389e
eigosan <aafanderson03@gmail.com>
2024-10-20 21:57:02 +0800

    setup

---
+++ atsvl/resources/js/Components/ResumeUploadForm.vue
@@ -0,0 +1,10 @@
+<template>
+
+    <form @submit.prevent="uploadResume">
+
+        <input type="file" @change="handleFileChange">
+        <button type="submit">Upload Resume</button>
+
+    </form>
+
+</template>
